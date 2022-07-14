c9f01dc233aed84eb8bfb959f092796a84cf7de2
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-14 16:06:44 +0200

    pushing to the GIT

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,9 +5,13 @@ const Directory = ({categories}) => {
     return(
         <div className='directory-container'>
             {categories.map((category) => (
-                <CategoryItem key={category.id} category={category}/>
+                <CategoryItem 
+                key={category.id} 
+                category={category}
+                />
             ))}
         </div>
+        
     )
 }
 
