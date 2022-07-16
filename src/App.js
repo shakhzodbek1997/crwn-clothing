ac2a65ddbc7c0d5ca0b33ac33d53e631e69ebc96
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-16 15:02:38 +0200

    Sign In page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.components';
 import Home from './routes/home/home.component';
 
 
@@ -13,8 +14,9 @@ const App = () => {
   return(
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index={true} element={<Home />}/>
+        <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />}/>
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.style.scss'
 
 const Navigation = () => {
     return(
         <Fragment>
             <div className='navigation'>
-                <Link className='logo-contaiiner' to='/'><div>Logo</div></Link>
+                <Link className='logo-container' to='/'>
+                    <CrwnLogo className='logo' />
+                </Link>
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'> Shop </Link>
+                    <Link className='nav-link' to='/sign-in'> Sign In </Link>
                 </div>
             </div>
             <Outlet/>
