0bb9f6bcf6009cfe4c11d503a138593bddb37374
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-21 13:56:50 +0200

    drop down cart is done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { ProductsProvider } from './contexts/products.context';
 import {UserProvider} from './contexts/user.context';
 
 import reportWebVitals from "./reportWebVitals";
+import { CartProvider } from './contexts/cart.context';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,7 +16,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App/>
+          <CartProvider>
+            <App/>
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,9 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import { CartContext } from '../../contexts/cart.context';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -9,6 +12,7 @@ import './navigation.style.scss'
 
 const Navigation = () => {
   const {currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
   
   return (
     <Fragment>
@@ -31,7 +35,9 @@ const Navigation = () => {
                 </Link>
               )
           }
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
