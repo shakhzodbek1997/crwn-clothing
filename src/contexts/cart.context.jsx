1f17fb04ae930d5abb3dad73a7f41035d8d36836
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-22 14:29:47 +0200

    checkout page, total price, removing all of them are Done!

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -45,7 +45,8 @@ export const CartContext = createContext({
     addItemToCart: () => {},
     removeItemFromCart: () => {},
     clearItemFromCart: () => {},
-    cartCount: 0
+    cartCount: 0,
+    cartTotal: 0
 });
 
 
@@ -53,11 +54,19 @@ export const CartProvider = ({children}) => {
     const[isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         setCartCount(newCartCount)
     }, [cartItems])
+
+    
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity  * cartItem.price, 0)
+        setCartTotal(newCartTotal )
+    }, [cartItems])
+
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
@@ -78,7 +87,8 @@ export const CartProvider = ({children}) => {
         removeItemToCart,
         clearItemFromCart,
         cartItems, 
-        cartCount
+        cartCount,
+        cartTotal
     }
 
     return (
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -5,7 +5,7 @@ import { CartContext } from '../../contexts/cart.context';
 import CheckoutItem from '../../components/checkout-item/checkout-item.componenrt';
 
 const Checkout = () => {
-    const {cartItems} = useContext(CartContext);
+    const {cartItems, cartTotal} = useContext(CartContext);
 
     return(
         <div className='checkout-container'>
@@ -29,7 +29,7 @@ const Checkout = () => {
             {cartItems.map((cartItem) => (
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />
             ))}
-            <span className='total'>Total: 0</span>
+            <span className='total'>Total: ${cartTotal}</span>
         </div>
     )
 }
