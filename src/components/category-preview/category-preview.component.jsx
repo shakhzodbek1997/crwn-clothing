e2b77d10acea5f6e6d45f47458dd41fdcaae6b4e
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-23 08:56:09 +0200

    categories page is done

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import ProductCard from '../product-card/product-card.component';
 import './category-preview.styles.scss';
 
@@ -5,12 +6,14 @@ const CategoryPreview = ({title, products}) => {
     return (
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>{title.toUppercase()}</span>
+                <Link className='title' to={title}>
+                    {title.toUpperCase()}
+                </Link>
             </h2>
             <div className='preview'>
                  {
                     products.filter((_, idx) => idx < 4 ) 
-                    .mqp((product) => 
+                    .map((product) => 
                         <ProductCard  key={product.id} product={product}/>
                     )
                  }
